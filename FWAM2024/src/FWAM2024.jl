4c88b697f7f4716ff19e0eb8b2db5341e7ce8e60
Will M. Farr <will.farr@ligo.org>
2024-10-23 09:35:21 -0400

    Plots from the Exact Model.

--- FWAM2024/src/FWAM2024.jl
+++ FWAM2024/src/FWAM2024.jl
@@ -3,6 +3,8 @@ module FWAM2024
 using AxisArrays
 using CairoMakie
 using Distributions
+using GaussianKDEs
+using LaTeXStrings
 using PairPlots
 using Random
 using SpecialFunctions
@@ -32,10 +34,6 @@ function plot_population_trace(axis, a, mu, sigma; extent=3, label=nothing, kwar
     lines!(axis, xs, exp.(p_ld.(xs)); label=label, kwargs...)
 end
 
-function plot_population_bands(axis, trace; extent=3, label=nothing)
-
-end
-
 @model function exact_model(xs)
     a_estimated = length(xs) / (sqrt(2*pi)*std(xs))
     a ~ Uniform(a_estimated/2, 2*a_estimated)
@@ -61,6 +59,21 @@ function do_exact_model(; seed = 8174570720888583473)
     f = pairplot(trace, PairPlots.Truth((; a=a_true, mu=mu_true, sigma=sigma_true)))
     save(joinpath(@__DIR__, "..", "figures", "exact_model_parameters.png"), f)
 
+    f = Figure()
+    a = Axis(f[1,1], xlabel=L"x", ylabel=L"\mathrm{d}N/\mathrm{d}x")
+    plot_population_trace(a, a_true, mu_true, sigma_true; color=:black, label="True")
+    k = KDE(xs)
+    x = minimum(xs):0.01*std(xs):maximum(xs)
+    lines!(a, x, pdf.((k,), x)*length(xs), color=:black, linestyle=:dash, label="Observed")
+    for i in 1:100
+        label = (i == 1 ? "Fitted" : nothing)
+        t = sample(trace, 1)
+        plot_population_trace(a, t[:a][1,1], t[:mu][1,1], t[:sigma][1,1]; color=Makie.wong_colors()[1], alpha=0.1, label=label)
+    end
+    axislegend(a, location=:bm)
+    save(joinpath(@__DIR__, "..", "figures", "exact_model_population.png"), f)
 end
+
+function do_
 
 end # module FWAM2024
